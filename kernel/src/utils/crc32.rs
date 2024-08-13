33c6e95c6fb9a3b8bfabbb0f3eaf268b8deaeaf9
BL-CZY <jiguangllsw@gmail.com>
2024-08-12 17:24:10 -0700

    crc32
    idk if it works or not, but reading ancient c is a mental torturing,
    especially rewriting it to rust

---
+++ kernel/src/utils/crc32.rs
@@ -0,0 +1,49 @@
+static mut I_TABLE: [u32; 256] = [0; 256];
+
+pub fn initialize_crc32() {
+    let i_polynomial: i32 = 0x04C11DB7;
+
+    for i_codes in 0..=0xFFu32 {
+        unsafe {
+            I_TABLE[i_codes as usize] = reflect(i_codes, 8) << 24;
+
+            for _ in 0..8 {
+                let temp = if I_TABLE[i_codes as usize] & (1 << 31) != 0 {
+                    i_polynomial
+                } else {
+                    0
+                };
+
+                I_TABLE[i_codes as usize] = (I_TABLE[i_codes as usize] << 1) ^ temp as u32;
+            }
+
+            I_TABLE[i_codes as usize] = reflect(I_TABLE[i_codes as usize], 32);
+        }
+    }
+}
+
+fn reflect(mut i_reflect: u32, c_char: i8) -> u32 {
+    let mut i_val: u32 = 0;
+
+    for i_pos in 0..(c_char + 1) {
+        if (i_reflect & 0x1) != 0 {
+            i_val |= 0x1 << (c_char - i_pos as i8);
+        }
+        i_reflect >>= 1;
+    }
+
+    i_val
+}
+
+fn partial_crc(i_crc: &mut u32, s_data: &[u8]) {
+    for data in s_data.iter() {
+        *i_crc = (*i_crc >> 8) ^ unsafe { I_TABLE[((*i_crc & 0xFF) ^ (*data) as u32) as usize] };
+    }
+}
+
+fn full_crc(s_data: &[u8]) -> u32 {
+    let mut ul_crc: u32 = 0xFFFFFFFF;
+    partial_crc(&mut ul_crc, s_data);
+
+    ul_crc ^ 0xFFFFFFFF
+}
--- kernel/src/utils/mod.rs
+++ kernel/src/utils/mod.rs
@@ -1,3 +1,5 @@
+pub mod crc32;
+
 pub fn binary_test(target: u64, bit: u64) -> bool {
     (target & (0x1 << bit)) == (0x1 << bit)
 }
