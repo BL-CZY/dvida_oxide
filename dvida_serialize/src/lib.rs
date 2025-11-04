cfd52b55085c8ef1b97b7bd7b220fcd227e6cf08
BL-CZY <jiguangllsw@gmail.com>
2025-11-04 11:38:11 +0000

    stuff

--- dvida_serialize/src/lib.rs
+++ dvida_serialize/src/lib.rs
@@ -1,2 +1,28 @@
+#[no_std]
+#[derive(Clone, Copy, Debug)]
+pub enum Endianness {
+    Little,
+    Big,
+    NA,
+}
 
+#[derive(Debug, Clone, Copy)]
+pub enum DvSerErr {}
 
+#[derive(Debug, Clone, Copy)]
+pub enum DvDeErr {}
+
+pub trait DvSerialize {
+    /// the serialize function takes in self, endianness, writes data to a slice of data
+    /// return the amount of bytes written
+    /// it will error if the buffer is too small
+    fn serialize(&self, endianness: Endianness, target: &mut [u8]) -> Result<usize, DvSerErr>;
+}
+
+pub trait DvDeserialize {
+    /// the deserialize function takes in endianness, a slice of data, and returns the parsed self
+    /// it will error if the conversion goes wrong
+    fn deserialize(endianness: Endianness, input: &[u8]) -> Result<Self, DvDeErr>
+    where
+        Self: Sized;
+}
--- dvida_serialize_macros/src/lib.rs
+++ dvida_serialize_macros/src/lib.rs
@@ -1,2 +1,26 @@
+use dvida_serialize::{DvSerErr, DvSerialize};
 
+extern crate proc_macro;
+use proc_macro::TokenStream;
 
+#[proc_macro_derive(DvSerialize)]
+pub fn derive_dv_serialize(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = &input.ident;
+    let generics = &input.generics;
+
+    // Input: struct Foo<T: Clone, U> where U: Debug { ... }
+    // Generates: impl<T: Clone, U> MyTrait for Foo<T, U> where U: Debug { ... }
+    //            ^^^^^ impl_generics   ^^^^ ty_generics  ^^^^^^^^^^^^^^ where_clause
+
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let expanded = quote! {
+        impl #impl_generics for #name #ty_genetics #where_clause {
+            fn serialize(&self, endianness: Endianness) {}
+        }
+    };
+
+    TokenStream::from(expanded)
+}
