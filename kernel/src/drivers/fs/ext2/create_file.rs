2e2588cec88418b719a0a444821176f0ba7b758c
BL-CZY <jiguangllsw@gmail.com>
2025-12-22 14:03:02 +0100

    refactor

--- kernel/src/drivers/fs/ext2/create_file.rs
+++ kernel/src/drivers/fs/ext2/create_file.rs
@@ -1,4 +1,4 @@
-use alloc::{boxed::Box, vec, vec::Vec};
+use alloc::{boxed::Box, vec::Vec};
 use dvida_serialize::{DvDeserialize, DvSerialize};
 
 use crate::{
@@ -14,11 +14,11 @@ use crate::{
 pub const RESERVED_BOOT_RECORD_OFFSET: i64 = 2;
 pub const BLOCK_SECTOR_SIZE: i64 = (BLOCK_SIZE as i64 / SECTOR_SIZE as i64) as i64;
 
-struct AllocatedBlock {
-    addr: i64,
+pub struct AllocatedBlock {
+    pub addr: i64,
     // relative to the block group
-    block_idx: i64,
-    gr_number: i64,
+    pub block_idx: i64,
+    pub gr_number: i64,
 }
 
 struct AllocatedInode {
@@ -37,60 +37,6 @@ impl Ext2Fs {
         )
     }
 
-    pub async fn traverse_find_blocks(
-        &self,
-        inode: &mut Inode,
-        mut cur_empty_block_in_inode: usize,
-        mut remaining_blocks: u8,
-    ) -> Result<Vec<AllocatedBlock>, HalFsOpenErr> {
-        let block_group_size = self.block_group_size();
-        let mut blocks_allocated = vec![];
-
-        for (group_number, addr) in (RESERVED_BOOT_RECORD_OFFSET..self.len() + 1)
-            .step_by(block_group_size as usize)
-            .enumerate()
-        {
-            if remaining_blocks <= 0 {
-                break;
-            }
-
-            let mut buf = Box::new([0u8; BLOCK_SIZE as usize]);
-
-            self.read_sectors(buf.clone(), addr + 2 * BLOCK_SECTOR_SIZE)
-                .await?;
-
-            let bit_iterator = BitIterator::new(buf.as_mut_slice());
-
-            for (idx, bit) in bit_iterator.into_iter().enumerate() {
-                if remaining_blocks <= 0 {
-                    break;
-                }
-
-                if bit == Bit::One {
-                    continue;
-                }
-
-                let block_addr = ((block_group_size * group_number as i64
-                    + RESERVED_BOOT_RECORD_OFFSET
-                    + 5
-                    + BLOCK_SECTOR_SIZE)
-                    + idx as i64) as u32;
-                remaining_blocks -= 1;
-                inode.i_block[cur_empty_block_in_inode] = block_addr;
-
-                blocks_allocated.push(AllocatedBlock {
-                    addr: block_addr as i64,
-                    block_idx: idx as i64,
-                    gr_number: group_number as i64,
-                });
-
-                cur_empty_block_in_inode += 1;
-            }
-        }
-
-        Ok(blocks_allocated)
-    }
-
     pub async fn allocated_blocks_for_inode(
         &self,
         inode: &mut Inode,
@@ -100,58 +46,14 @@ impl Ext2Fs {
             return Err(HalFsOpenErr::Unsupported);
         }
 
-        let mut blocks_allocated = vec![];
+        let blocks_allocated = self
+            .allocate_n_blocks_in_group(group_number, self.super_block.s_prealloc_blocks as usize)
+            .await?;
 
-        let mut remaining_blocks = self.super_block.s_prealloc_blocks;
-
-        let block_group_size =
-            self.super_block.s_blocks_per_group as i64 * (BLOCK_SIZE as i64 / SECTOR_SIZE as i64);
-
-        let addr =
-            block_group_size * group_number + RESERVED_BOOT_RECORD_OFFSET + 2 * BLOCK_SECTOR_SIZE;
-
-        let mut buf = Box::new([0u8; BLOCK_SIZE as usize]);
-
-        self.read_sectors(buf.clone(), addr).await?;
-
-        let bit_iterator: BitIterator<u8> = BitIterator::new(buf.as_mut_slice());
-
-        let mut cur_empty_block_in_inode = 0;
-        for (idx, bit) in bit_iterator.into_iter().enumerate() {
-            if remaining_blocks <= 0 {
-                break;
-            }
-
-            if bit == Bit::One {
-                continue;
-            }
-
-            let block_addr = ((block_group_size * group_number
-                + RESERVED_BOOT_RECORD_OFFSET
-                + 5
-                + BLOCK_SECTOR_SIZE)
-                + idx as i64) as u32;
-
-            remaining_blocks -= 1;
-            inode.i_block[cur_empty_block_in_inode] = block_addr;
-            cur_empty_block_in_inode += 1;
-            blocks_allocated.push(AllocatedBlock {
-                addr: block_addr as i64,
-                block_idx: idx as i64,
-                gr_number: group_number,
-            });
+        for block in blocks_allocated.iter() {
+            inode.i_block[inode.i_blocks as usize] = block.addr as u32;
+            inode.i_blocks += 1;
         }
-
-        if remaining_blocks <= 0 {
-            return Ok(blocks_allocated);
-        }
-
-        // iterate over the entire filesystem
-        blocks_allocated.extend(
-            self.traverse_find_blocks(inode, cur_empty_block_in_inode, remaining_blocks)
-                .await?
-                .into_iter(),
-        );
 
         Ok(blocks_allocated)
     }
--- kernel/src/drivers/fs/ext2/write.rs
+++ kernel/src/drivers/fs/ext2/write.rs
@@ -1,11 +1,121 @@
-use alloc::boxed::Box;
+use alloc::{boxed::Box, vec, vec::Vec};
 
 use crate::{
-    drivers::fs::ext2::{BLOCK_SIZE, Inode, read::Progress, structs::Ext2Fs},
-    hal::fs::{HalFsIOErr, HalIOCtx},
+    crypto::iterators::{Bit, BitIterator},
+    drivers::fs::ext2::{
+        BLOCK_SIZE, Inode,
+        create_file::{AllocatedBlock, BLOCK_SECTOR_SIZE, RESERVED_BOOT_RECORD_OFFSET},
+        read::Progress,
+        structs::Ext2Fs,
+    },
+    hal::{
+        fs::{HalFsIOErr, HalIOCtx},
+        storage::{HalStorageOperationErr, SECTOR_SIZE},
+    },
 };
 
 impl Ext2Fs {
+    pub async fn allocate_n_blocks(
+        &self,
+        mut remaining_blocks: usize,
+    ) -> Result<Vec<AllocatedBlock>, HalStorageOperationErr> {
+        let block_group_size = self.block_group_size();
+        let mut blocks_allocated = vec![];
+
+        for (group_number, addr) in (RESERVED_BOOT_RECORD_OFFSET..self.len() + 1)
+            .step_by(block_group_size as usize)
+            .enumerate()
+        {
+            if remaining_blocks <= 0 {
+                break;
+            }
+
+            let mut buf = Box::new([0u8; BLOCK_SIZE as usize]);
+
+            self.read_sectors(buf.clone(), addr + 2 * BLOCK_SECTOR_SIZE)
+                .await?;
+
+            let bit_iterator = BitIterator::new(buf.as_mut_slice());
+
+            for (idx, bit) in bit_iterator.into_iter().enumerate() {
+                if remaining_blocks <= 0 {
+                    break;
+                }
+
+                if bit == Bit::One {
+                    continue;
+                }
+
+                let block_addr = ((block_group_size * group_number as i64
+                    + RESERVED_BOOT_RECORD_OFFSET
+                    + 5
+                    + BLOCK_SECTOR_SIZE)
+                    + idx as i64) as u32;
+                remaining_blocks -= 1;
+
+                blocks_allocated.push(AllocatedBlock {
+                    addr: block_addr as i64,
+                    block_idx: idx as i64,
+                    gr_number: group_number as i64,
+                });
+            }
+        }
+
+        Ok(blocks_allocated)
+    }
+
+    pub async fn allocate_n_blocks_in_group(
+        &self,
+        group_number: i64,
+        mut num: usize,
+    ) -> Result<Vec<AllocatedBlock>, HalStorageOperationErr> {
+        let mut blocks_allocated = vec![];
+
+        let block_group_size =
+            self.super_block.s_blocks_per_group as i64 * (BLOCK_SIZE as i64 / SECTOR_SIZE as i64);
+
+        let addr =
+            block_group_size * group_number + RESERVED_BOOT_RECORD_OFFSET + 2 * BLOCK_SECTOR_SIZE;
+
+        let mut buf = Box::new([0u8; BLOCK_SIZE as usize]);
+
+        self.read_sectors(buf.clone(), addr).await?;
+
+        let bit_iterator: BitIterator<u8> = BitIterator::new(buf.as_mut_slice());
+
+        for (idx, bit) in bit_iterator.into_iter().enumerate() {
+            if num <= 0 {
+                break;
+            }
+
+            if bit == Bit::One {
+                continue;
+            }
+
+            let block_addr = ((block_group_size * group_number
+                + RESERVED_BOOT_RECORD_OFFSET
+                + 5
+                + BLOCK_SECTOR_SIZE)
+                + idx as i64) as u32;
+
+            num -= 1;
+            blocks_allocated.push(AllocatedBlock {
+                addr: block_addr as i64,
+                block_idx: idx as i64,
+                gr_number: group_number,
+            });
+        }
+
+        if num <= 0 {
+            return Ok(blocks_allocated);
+        }
+
+        // iterate over the entire filesystem
+        blocks_allocated.extend(self.allocate_n_blocks(num).await?.into_iter());
+
+        Ok(blocks_allocated)
+    }
+
     async fn write_till_next_block(
         &mut self,
         inode: &mut Inode,
