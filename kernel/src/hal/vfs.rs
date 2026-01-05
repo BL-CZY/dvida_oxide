8e38cc1b2c61ffd2623008ba9ca6da7a137e5f87
BL-CZY <jiguangllsw@gmail.com>
2026-01-05 15:00:44 +0100

    vfs thread

--- kernel/src/hal/vfs.rs
+++ kernel/src/hal/vfs.rs
@@ -1,10 +1,12 @@
 use alloc::boxed::Box;
+use ejcineque::sync::mpsc::unbounded::{UnboundedSender, unbounded_channel};
+use once_cell_no_std::OnceCell;
 use terminal::log;
 
 use crate::{
     drivers::fs::ext2::structs::Ext2Fs,
     hal::{
-        fs::{FILE_SYSTEM, HalFsIOErr, HalIOCtx, HalInode, OpenFlags},
+        fs::{FILE_SYSTEM, FileSystem, HalFsIOErr, HalIOCtx, HalInode, OpenFlags},
         path::Path,
         storage::read_gpt,
     },
@@ -18,6 +20,35 @@ pub enum OpenErr {
     IsADirectory = -21,
     TooManyOpenFiles = -24,
     Unknown = -128,
+}
+
+pub enum VfsOperationType {}
+
+pub struct VfsOperation {
+    operation_type: VfsOperationType,
+}
+
+pub static VFS_SENDER: OnceCell<UnboundedSender<VfsOperation>> = OnceCell::new();
+
+pub async fn spawn_vfs_task(drive_id: usize, entry_idx: usize) {
+    let mut fs = FileSystem::default();
+
+    let (_header, mut entries) = read_gpt(drive_id).await.expect("Failed to read GPT");
+    log!("Root directory entry: {:?}", entries[entry_idx]);
+
+    fs.drive_id = drive_id;
+    fs.entry_idx = entry_idx;
+    fs.entry = entries.remove(entry_idx);
+
+    // only ext2 is supported
+    fs.fs_impl = crate::hal::fs::HalFs::Ext2(Ext2Fs::new(drive_id, fs.entry.clone()).await);
+
+    let (tx, rx) = unbounded_channel::<VfsOperation>();
+    VFS_SENDER.set(tx).expect("Failed to set vfs task sender");
+
+    while let Some(operation) = rx.recv().await {
+        match operation.operation_type {}
+    }
 }
 
 pub async fn init_vfs(drive_id: usize, entry_idx: usize) {
